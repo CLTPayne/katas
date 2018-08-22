b66a7e1597a908cb1f6822952b10ed378ee83854
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-08-22 17:03:31 +0100

    Add named private functions
    * Increase readability of conditionals

--- leap-years/lib/leap_year.rb
+++ leap-years/lib/leap_year.rb
@@ -3,12 +3,26 @@ class LeapYear
   def initialize
   end
 
-  def calculate(number)
-    if number % 400 === 0 || number % 4 === 0 && number % 100 != 0
+  def calculate(num)
+    if is_divisible_by_400?(num) || is_divisible_by_4?(num) && !is_divisible_by_100?(num)
       "Yes that's a leap year! Enjoy Feb 29th!"
-    elsif number % 4 != 0 || number % 100 === 0 && number % 400 != 0
+    elsif !is_divisible_by_4?(num) || is_divisible_by_100?(num) && !is_divisible_by_400?(num)
       "No! Sorry, no extra day for you!"
     end
+  end
+
+  private
+
+  def is_divisible_by_400?(num)
+    num % 400 === 0
+  end
+
+  def is_divisible_by_4?(num)
+    num % 4 === 0
+  end
+
+  def is_divisible_by_100?(num)
+    num % 100 === 0
   end
 
 end
